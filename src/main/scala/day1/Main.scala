f8f10ec6a45f4fc55850103e3952c32effb52c01
nathan-rees <nathan_rees@live.co.uk>
2020-03-03 12:55:58 +0000

    day2

--- src/main/scala/day1/Main.scala
+++ src/main/scala/day1/Main.scala
@@ -2,9 +2,7 @@ package day1
 
 object Main {
 
-  def main(args: Array[String]): Unit = {
-    println(addSmallMultBy(1,4,7))
-  }
+
 
   def num(A:Int,B:Int,C:Int): Unit ={
 
@@ -15,10 +13,6 @@ object Main {
       println(num-1)
     }
   }
-def addSmallMultBy(a:Int,b:Int,c:Int): Int ={
-  val nums=List(a,b,c).sorted
-  nums[0]+nums[1]*(nums[2])
-  List.n
-}
+
 
 }
---
+++ src/main/scala/day2/ReduceLeft.scala
@@ -0,0 +1,38 @@
+package day2
+
+object ReduceLeft extends App {
+  println(mult(7,"5"))
+  println(strings2("hal","lo",'a','e').toString())
+  println(sum(12,12,false))
+  pasd("hi",4)
+
+  def output[A](x:A): A ={
+    x
+  }
+
+  def mult(x:Int,y:String): Int ={
+    x*y.toInt
+  }
+
+  def strings2(a:String,b:String,c: Char,d:Char) ={
+  var s=new StringBuilder(a);
+    s.append(b)
+    var ss=new StringBuilder("")
+
+    s.map(x=>if(x==c){ss+=d}else{ss+=x})
+  ss.toString()
+  }
+def sum(a:Int,b:Int,c:Boolean): Int ={
+
+  if(c==true){return(a+b)}
+  return a*b
+}
+  def pasd(s:String,num: Int): Unit ={
+    for(i<- 1 to num){
+      println(s)
+    }
+  }
+
+
+}
+
